bc2c5a4ebf51655cb27432b27a081d08988e3244
Evgeny Nazarchuk <evgsvg@yandex.ru>
2021-09-12 04:35:41 +0300

    check parallel

--- TestDbGeneration.IntegrationTest/UnitTest2.cs
+++ TestDbGeneration.IntegrationTest/UnitTest2.cs
@@ -1,216 +1,248 @@
 using System;
 using System.Threading.Tasks;
-using Xunit;
+//using Xunit;
+using NUnit.Framework;
 using TestDbGeneration.IntegrationTest.Support;
 
 namespace TestDbGeneration.IntegrationTest
 {
+    [Parallelizable(ParallelScope.All)]
     public class UnitTest2
     {
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test1()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test2()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test3()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test4()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test5()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test6()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test7()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test8()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test9()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test10()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test11()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test12()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test13()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test14()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test15()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test16()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test17()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test18()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test19()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test20()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test21()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test22()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test23()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test24()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test25()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test26()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test27()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test28()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test29()
         {
             using var env = new TestEnvironment();
             var result = await env.HttpClient.GetAsync("person");
         }
 
-        [Fact]
+        [Test]
+        [Parallelizable]
         public async Task Test30()
         {
             using var env = new TestEnvironment();
